afb663e2a5a7136fb3c99d467e31405c588f0aa2
Ian Cordle <ianvalentino@gmail.com>
2020-06-09 23:19:31 -0400

    Updates

---
+++ _next/static/fLAxuw7wgTt1KMFUvRrL5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/404":[a],"/index":[a],"/markdown-reference":[a],"/synth":[a]}}("static\u002Fchunks\u002F22f6c79baf5c529e9999c8038f9c3d874a58ef27.d75bc904ae3773188c41.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
