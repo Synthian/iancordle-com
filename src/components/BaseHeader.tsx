3737f6b575b5a2ab56d8b0078abc65cb7a3a2270
Ian Cordle <ianvalentino@gmail.com>
2020-06-10 19:49:56 -0400

    Try to fix the deploy action

--- src/components/BaseHeader.tsx
+++ src/components/BaseHeader.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Flex, useTheme } from '@chakra-ui/core';
-import { PALETTE } from '../styles/theme';
 
 export interface BaseHeaderProps {
   maxW: string;
@@ -12,17 +11,7 @@ const BaseHeader: React.FC<BaseHeaderProps> = (props) => {
   const theme = useTheme();
 
   return (
-    <Flex
-      position={'sticky'}
-      bg={props.bg}
-      zIndex={theme.zIndices.sticky}
-      top={0}
-      maxW={props.maxW}
-      px={props.px}
-      py={4}
-      margin={'auto'}
-      alignItems={'center'}
-    >
+    <Flex position={'sticky'} bg={props.bg} zIndex={theme.zIndices.sticky} top={0} maxW={props.maxW} px={props.px} py={4} margin={'auto'} alignItems={'center'}>
       {props.children}
     </Flex>
   );
